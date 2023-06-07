4620d76f2710d79e99223c7d8f6041d9fd715f87
Tom MBOYA <tommboya044@gmail.com>
2023-06-08 00:18:59 +0300

    output and input  2

---
+++ src/main/java/org/example/PrintYourNameAndSurname.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        String name = "Tom";
+        String surname = "Mboya";
+        System.out.println(name + " " + surname);
+    }
+}
